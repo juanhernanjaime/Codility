f7334c6158fcd65da3d49e09df89706ce5063357
juan <juajaiar@hotmail.com>
2021-01-08 10:37:20 +0100

    Simply ajust the code

--- src/main/java/CodilityDemo.java
+++ src/main/java/CodilityDemo.java
@@ -1,14 +1,11 @@
 import java.util.stream.IntStream;
 
 public class CodilityDemo {
-
-    public static int solution(int[] A) {
+    public int solution(int[] A) {
         return IntStream.of(A)
                 .filter((i) -> i > 0)
                 .sorted()
                 .distinct()
-                .reduce(0, (a, b) -> {
-                    return a + 1 != b ? a : b;
-                }) + 1;
+                .reduce(0, (a, b) -> a + 1 != b ? a : b) + 1;
     }
 }
